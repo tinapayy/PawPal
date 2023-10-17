c3ff663c2182e36aa30a687701fa6354cdbcca0e
Chester Ken Gallego <chesterkengallego@gmail.com>
2023-10-17 22:14:53 +0800

    Implement sign up and sign in functions

---
+++ FirebaseConfig.ts
@@ -0,0 +1,14 @@
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAE_w2PYZUdG6RcOmCMxuJgw-5MOtfj0uE',
+  authDomain: 'pawpal-bf0d3.firebaseapp.com',
+  projectId: 'pawpal-bf0d3',
+  storageBucket: 'pawpal-bf0d3.appspot.com',
+  messagingSenderId: '747495959390',
+  appId: '1:747495959390:web:91528f916887411ff33dcf',
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);

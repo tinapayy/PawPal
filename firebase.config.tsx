5058aca60f2dcd747a2d996d6d8bbf0f2ffb48d4
JeffBat-Original <batogjeffrouzel@gmail.com>
2024-01-09 06:56:58 +0800

    Update Firebase authentication configuration to remove the warning during sign up and sign in

--- firebase.config.tsx
+++ firebase.config.tsx
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from 'firebase/app';
-import {getAuth} from 'firebase/auth';
+// import {getAuth} from 'firebase/auth';
+import {initializeAuth, getReactNativePersistence} from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import {getFirestore} from 'firebase/firestore';
 import {getStorage, ref} from 'firebase/storage';
 
@@ -19,6 +21,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+// export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
 export const FIREBASE_STORAGE = getStorage(FIREBASE_APP);

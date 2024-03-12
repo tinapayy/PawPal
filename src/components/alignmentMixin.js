97ba1758adbe057e6bd9a00bb4cfd6e41a4f3ee4
Arianne Deaniele <100850374+daneeeeeee@users.noreply.github.com>
2024-03-12 20:22:42 +0800

    alignment mixin and small edit on the AddClinicDetails page for code cleanup later on.

---
+++ src/components/alignmentMixin.js
@@ -0,0 +1,9 @@
+export const alignmentMixin = {
+  alingnment: {
+    alignItems: 'center',
+    alignContent: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row'
+  },
+};

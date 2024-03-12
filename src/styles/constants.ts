15e6e768b02503e387344e3caa2918d3da35aca7
tinapayy <kvcelis@up.edu.ph>
2024-03-12 09:59:16 +0800

    Create constants.ts

---
+++ src/styles/constants.ts
@@ -0,0 +1,23 @@
+const constants = {
+  // COLORS
+  $primaryColor: '#ffac4e', // orange
+  $secondaryColor: '#5a2828', // brown
+  $tertiaryColor: '#ffffff', // white
+  $quaternaryColor: '#ffba69', // light orange
+  $accentColor: '#ff6464', // pink
+
+  $textColor: '#000000', // black
+  $backgroundColor: '#ffffff', // white
+
+  // SPACING
+  $spacingSmall: 5,
+  $spacingMedium: 10,
+  $spacingLarge: 20,
+
+  // FONT SIZES
+  $fontSizeSmall: 14,
+  $fontSizeMedium: 16,
+  $fontSizeLarge: 20,
+};
+
+export default constants;

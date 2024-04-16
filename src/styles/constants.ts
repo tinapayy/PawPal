b3857d8256f4aa5ef1ef7eae95429558d840f427
Gliezel Ann Pajarilla <gtpajarilla@up.edu.ph>
2024-04-16 11:08:19 +0800

    changes made to the chat

--- src/styles/constants.ts
+++ src/styles/constants.ts
@@ -6,15 +6,20 @@ const constants = {
   $quaternaryColor: '#ffba69', // light orange
   $quinaryColor: '#f1d5c6',  // light grayish orange
   $senaryColor: "#ff8d4d",  // dark orange
+  $septenaryColor: "#aaaa", //gray
+  $octonaryColor: '#ff8700',
 
 
   $accentColor: '#ff6464', // pink
   $accentColor1: '#894848', // light brown
 
+
   $textColor1: '#000000', // black
   $textColor2: '#ffffff', // white
   $backgroundColor: '#ffffff', // white
-
+  $backgroundColor1: '#ffac4e', // light orange
+  $backgroundColor2: 'rgba(255, 186, 105, 0.28)',
+  $backgroundColor3: 'rgba(255, 184, 105, 0.10)',
   // SPACING
   $spacingLetter: 0.25,
   $spacingSmall: 5,
@@ -39,6 +44,7 @@ const constants = {
   $fontFamilyMedium: 'Poppins-Medium',
   $fontFamilySemiBold: 'Poppins-SemiBold',
   $fontFamilyLight: 'Poppins-Light',
+  $fontFamilyRegular: 'Poppins-Regular',
   $fontFamilyThin: 'Poppins-Thin',
   $fontFamilyExtraBold: 'Poppins-ExtraBold',
   $fontFamilyExtraLight: 'Poppins-ExtraLight',

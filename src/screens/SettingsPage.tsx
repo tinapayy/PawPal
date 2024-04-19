22d0f55ce3a1dbeb75a07112eeddefd9942ea0b3
Arianne Deaniele <100850374+daneeeeeee@users.noreply.github.com>
2024-04-19 20:20:18 +0800

    ui fixes

--- src/screens/SettingsPage.tsx
+++ src/screens/SettingsPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, ViewStyle } from 'react-native';
 
 const grh = require('../images/grh.png');
 import LogOutModal from './LogoutModal';
@@ -12,6 +12,7 @@ import * as icons from '../imports/icons/icons';
 import constants from '../styles/constants';
 import {buttonMixin} from '../components/buttonMixin';
 import { alignmentMixin } from '../components/alignmentMixin';
+import {useNavigateTo} from '../components/navigation';
 
 function SettingsPage() {
   const navigation = useNavigation();
@@ -22,13 +23,16 @@ function SettingsPage() {
     if (!user) {
       navigation.reset({
         index: 0,
-        routes: [{ name: 'GettingStarted2' }],
+        routes: [{ name: 'GettingStarted2' } as any],
       });
     }
   });
 
   const [deleteAccountModal, setDeleteAccountModal] = useState(false);
   const [logoutModal, setLogoutModal] = useState(false);
+  const ProfileDetails = useNavigateTo('ProfileDetails');
+  const EditUserProfile = useNavigateTo('EditUserProfile');
+  const ChoosePet = useNavigateTo('ChoosePet');
 
   return (
     <View style={styles.container}>
@@ -40,9 +44,7 @@ function SettingsPage() {
         <Image source={grh} style={styles.headerimg} />
       </View>
       <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('ProfileDetails');
-        }}>
+        onPress={ProfileDetails}>
         <FontAwesomeIcon
           // icon={icons.faArrowLeft}
           icon={icons.faArrowLeft}
@@ -54,13 +56,13 @@ function SettingsPage() {
       <View style={styles.buttonContainer}>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('EditUserProfile')}>
+          onPress={EditUserProfile}>
           <icons.User size={30} color="#FF8700" strokeWidth={3} />
           <Text style={styles.buttonText}>Edit User Profile</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('ChoosePet')}>
+          onPress={ChoosePet}>
           <icons.ClinicIcon size={30} color="#FF8700" strokeWidth={3} />
           <Text style={styles.buttonText}>Edit Pet Profile</Text>
         </TouchableOpacity>
@@ -101,13 +103,13 @@ const styles = StyleSheet.create({
     backgroundColor: constants.$senaryColor,
     borderBottomLeftRadius: 40,
     borderBottomRightRadius: 40,
-  },
+  } as ViewStyle,
   headertextandicon: {
     ...alignmentMixin.alignment1,
     marginLeft: '10%',
     paddingRight: '10%',
     marginRight: '15%',
-  },
+  } as ViewStyle,
   headerText: {
     paddingLeft: '3%',
     fontSize: 35,
@@ -131,22 +133,22 @@ const styles = StyleSheet.create({
     ...buttonMixin.settingsButton,
     justifyContent: undefined,
     backgroundColor: constants.$backgroundColor,
-  },
+  } as ViewStyle,
   buttonlogout: {
     ...alignmentMixin.alignment1,
     ...buttonMixin.settingsButton,
     justifyContent: undefined,
     backgroundColor: constants.$primaryColor,
-  },
+  } as ViewStyle,
   buttonText: {
     ...buttonMixin.buttonText,
     ...buttonMixin.settingsButtonText,
     color: constants.$senaryColor,
-  },
+  } as ViewStyle,
   buttonlogoutText: {
     ...buttonMixin.buttonText,
     ...buttonMixin.settingsButtonText,
-  },
+  } as ViewStyle,
 });
 
 export default SettingsPage;
